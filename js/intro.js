d3272fbf6dcc9bdd209bfbc2c32c0380c42a1730
Dalton Tarde <155396632+datapips@users.noreply.github.com>
2024-05-13 22:10:27 -0700

    Update intro.js

--- js/intro.js
+++ js/intro.js
@@ -1,76 +1,58 @@
-document.addEventListener('DOMContentLoaded', () => {
-    let header = document.querySelector('#intro');
-    if (!header) {
-        console.error('Header element not found!');
-        return;
+let header = document.querySelector('#intro');
+let anim = [
+    { t: " ", ms: 200 },
+    { t: "_", ms: 200 },
+    { t: " ", ms: 200 },
+    { t: "_", ms: 200 },
+    { t: "T_", ms: 100 },
+    { t: "TU_", ms: 100 },
+    { t: "TUS_", ms: 100 },
+    { t: "TUSH_", ms: 100 },
+    { t: "TUSHA_", ms: 100 },
+    { t: "TUSHAR_", ms: 100 },
+    { t: "TUSHAR _", ms: 100 },
+    { t: "TUSHAR M_", ms: 100 },
+    { t: "TUSHAR MU_", ms: 100 },
+    { t: "TUSHAR MUR_", ms: 100 },
+    { t: "TUSHAR MURA_", ms: 100 },
+    { t: "TUSHAR MURAL_", ms: 100 },
+    { t: "TUSHAR MURALI_", ms: 100 },
+    { t: "TUSHAR MURALID_", ms: 100 },
+    { t: "TUSHAR MURALIDH_", ms: 100 },
+    { t: "TUSHAR MURALIDHA_", ms: 100 },
+    { t: "TUSHAR MURALIDHAR_", ms: 100 },
+    { t: "TUSHAR MURALIDHARA_", ms: 100 },
+    { t: "TUSHAR MURALIDHARAN_", ms: 100 },
+    { t: "TUSHAR MURALIDHARAN\u00A0", ms: 200 },
+    { t: "TUSHAR MURALIDHARAN_", ms: 200 },
+    { t: "TUSHAR MURALIDHARAN\u00A0", ms: 200 },
+    { t: "TUSHAR MURALIDHARAN_", ms: 200 },
+    { t: "TUSHAR MURALIDHARAN", ms: 200 },
+    { t: "TUSHAR MURALIDHARAN", ms: 200 }
+];
+
+let stepDenominator = 1;
+if (window.localStorage.stepDenominator)
+    stepDenominator = window.localStorage.stepDenominator;
+
+let i = 0;
+header.style.width = "30vw"
+let update = () => {
+    let step = anim[i];
+    header.innerText = step.t;
+    i++;
+
+    if (i < anim.length)
+        setTimeout(update, step.ms / stepDenominator);
+    else {
+        header.classList.add('top');
+        setTimeout(() => {
+            document.getElementById('main').style.opacity = 1;
+            header.style.position = "relative";
+            header.style.width = "100%"
+            anim1();
+        }, 500);
+        window.localStorage.stepDenominator = 2;
     }
-
-    let anim = [
-        { t: " ", ms: 200 },
-        { t: "_", ms: 200 },
-        { t: " ", ms: 200 },
-        { t: "_", ms: 200 },
-        { t: "D_", ms: 100 },
-        { t: "DA_", ms: 100 },
-        { t: "DAL_", ms: 100 },
-        { t: "DALT_", ms: 100 },
-        { t: "DALTO_", ms: 100 },
-        { t: "DALTON_", ms: 100 },
-        { t: "DALTON _", ms: 100 },
-        { t: "DALTON T_", ms: 100 },
-        { t: "DALTON TA_", ms: 100 },
-        { t: "DALTON TAR_", ms: 100 },
-        { t: "DALTON TARD_", ms: 100 },
-        { t: "DALTON TARDE_", ms: 100 },
-        { t: "DALTON TARDE\u00A0", ms: 200 },
-        { t: "DALTON TARDE_", ms: 200 },
-        { t: "DALTON TARDE\u00A0", ms: 200 },
-        { t: "DALTON TARDE_", ms: 200 },
-        { t: "DALTON TARDE", ms: 200 },
-        { t: "DALTON TARDE", ms: 200 }
-    ];
-
-    let stepDenominator = 1;
-    try {
-        if (window.localStorage.stepDenominator) {
-            stepDenominator = parseFloat(window.localStorage.stepDenominator);
-        }
-    } catch (e) {
-        console.warn('Local storage access error:', e);
-    }
-
-    let i = 0;
-    header.style.width = "30vw";
-
-    let update = () => {
-        if (i < anim.length) {
-            let step = anim[i];
-            header.innerText = step.t;
-            i++;
-            setTimeout(update, step.ms / stepDenominator);
-        } else {
-            header.classList.add('top');
-            setTimeout(() => {
-                let mainElement = document.getElementById('main');
-                if (mainElement) {
-                    mainElement.style.opacity = 1;
-                }
-                header.style.position = "relative";
-                header.style.width = "100%";
-                
-                if (typeof initGlobe === 'function') {
-                    initGlobe();
-                } else {
-                    console.error('initGlobe function not defined!');
-                }
-
-                try {
-                    window.localStorage.stepDenominator = 2;
-                } catch (e) {
-                    console.warn('Local storage access error:', e);
-                }
-            }, 500);
-        }
-    };
-    update();
-});
+}
+update();
