c40ee9c69b198ad5652a723989f60a87d3d58d15
Dalton Tarde <155396632+datapips@users.noreply.github.com>
2024-05-13 19:46:20 -0700

    Update intro.js

--- js/intro.js
+++ js/intro.js
@@ -1,24 +1,17 @@
 let header = document.querySelector('#intro');
 let anim = [
-    { t: "{ }", ms: 200 },
-    { t: "{_}", ms: 200 },
-    { t: "{ }", ms: 200 },
-    { t: "{_}", ms: 200 },
-    { t: "{O_}", ms: 100 },
-    { t: "{OT_}", ms: 100 },
-    { t: "{OTT_}", ms: 100 },
-    { t: "{OTTO_}", ms: 100 },
-    { t: "{OTTOM_}", ms: 100 },
-    { t: "{OTTOMA_}", ms: 100 },
-    { t: "{OTTOMAT_}", ms: 100 },
-    { t: "{OTTOMATE_}", ms: 100 },
-    { t: "{OTTOMATED_}", ms: 100 },
-    { t: "{OTTOMATED }", ms: 200 },
-    { t: "{OTTOMATED_}", ms: 200 },
-    { t: "{OTTOMATED }", ms: 200 },
-    { t: "{OTTOMATED_}", ms: 200 },
-    { t: "{OTTOMATED}", ms: 200 },
-    { t: "{OTTOMATED}", ms: 200 }
+    { t: "D", ms: 200 },
+    { t: "Da", ms: 200 },
+    { t: "Dal", ms: 200 },
+    { t: "Dalt", ms: 200 },
+    { t: "Dalto}", ms: 100 },
+    { t: "Dalton", ms: 100 },
+    { t: "Dalton ", ms: 100 },
+    { t: "Dalton T", ms: 100 },
+    { t: "Dalton Ta", ms: 100 },
+    { t: "Dalton Tar", ms: 100 },
+    { t: "Dalton Tard", ms: 100 },
+    { t: "Dalton Tarde", ms: 100 },
 ];
 let stepDenominator = 1;
 if (window.localStorage.stepDenominator)
