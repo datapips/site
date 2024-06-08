610fa39175fdb3c3f4e306cc04ccead0185b3c6c
Dalton Tarde <155396632+datapips@users.noreply.github.com>
2024-06-07 20:29:53 -0700

    Update globe.js

--- js/globe.js
+++ js/globe.js
@@ -24,16 +24,6 @@ function animate() {
 let initGlobe = () => {
     globe.init();
     animate();
-    /*
-    fetch('https://ip-api.io/json').then(r => r.text()).then(r => {
-        let loc = JSON.parse(r);
-        globe.addMarker(loc.latitude, loc.longitude, loc.ip);
-        fetch('https://ip-api.io/json/209.182.233.230').then(r => r.text()).then(r => {
-            let loc2 = JSON.parse(r);
-            globe.addMarker(loc2.latitude, loc2.longitude, loc2.ip, Math.abs(loc.lon - loc2.lon) > 25);
-        });
-    });
-    */
     var constellation = [];
     var opts = {
         coreColor: "#75E6DA",
